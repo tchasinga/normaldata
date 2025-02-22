b0f99196295c556563d899716d666c42c2ebd0d0
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-22 11:45:17 +0300

    feat: implement user redirection and display current user in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-export default function page() {
+
+export default function Dashboard() {
+ 
+  const currentUser = useSelector(
+    (state: any) => state.user && state.user.user.currentUser
+  );
+
+
   return (
     <div>
+
         <h1>this is my dashboard</h1>
+        <h1>{currentUser?.user?.name}</h1>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import Dashboard from "./dashboard/page";
 import Signin from "./(formgroup)/signin/page";
 
+interface RootState {
+  user: {
+    user: {
+      currentUser: any;
+    };
+  };
+}
 
 export default function Home() {
+  const router = useRouter();
+  const currentUser = useSelector((state: RootState) => state.user.user.currentUser);
+
+  if (!currentUser) {
+    router.push('/signin');
+    return null;
+  }
+
   return (
     <div className="max-w-screen-xl mx-auto px-5">
-       <Signin />
+        {
+          currentUser ? <Dashboard /> : <Signin />
+        }
     </div>
   );
 }
