18245af86da95df0bdb60359a49b0384331e7327
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-22 12:37:50 +0300

    feat: add user redirection in dashboard to enforce authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useRouter } from 'next/navigation';
 
 export default function Dashboard() {
   const currentUser = useSelector((state: any) => state.user && state.user.user.currentUser);
+    const router = useRouter();
+  
+    useEffect(() => {
+      if (!currentUser?.user) {
+        router.push("/signin");
+      }
+    }, [currentUser, router]);
 
   return (
     <div>
